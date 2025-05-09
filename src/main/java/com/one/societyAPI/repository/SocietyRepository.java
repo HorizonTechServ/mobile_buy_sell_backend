bb317205e8c65de7bbf741c31f626408a3922559
vimal8086 <86038901+vimal8086@users.noreply.github.com>
2025-05-09 15:55:15 +0530

    Cleanup

--- src/main/java/com/one/societyAPI/repository/SocietyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.one.societyAPI.repository;
-
-import com.one.societyAPI.entity.Society;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SocietyRepository extends JpaRepository<Society, Long> {
-}
